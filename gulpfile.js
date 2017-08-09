cacb1513d14b737735679e6f0d3cf05d9980522b
Isaac Murchie <isaac@saucelabs.com>
2017-08-09 09:16:17 -0400

    Generate coverage files?

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,9 @@ let gulp = require('gulp'),
     boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
-  build: 'appium-xcuitest-driver'
+  build: 'appium-xcuitest-driver',
+  coverage: {
+    files: ['./test/**/*-specs.js', '!./test/e2e/**'],
+    verbose: true
+  },
 });
